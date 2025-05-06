a43d35eab5e79717b63a40a43df4e031dca701f8
sabbircse499 <sabbir472003@gmail.com>
2025-05-06 23:25:18 +0600

    course section

--- lib/view/course/course.dart
+++ lib/view/course/course.dart
@@ -1,22 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:sabbirme/view/animation/typewriter.dart';
-class course extends StatefulWidget {
-  const course({super.key});
+
+class Course extends StatefulWidget {
+  const Course({super.key});
 
   @override
-  State<course> createState() => _courseState();
+  State<Course> createState() => _CourseState();
 }
 
-class _courseState extends State<course> {
+class _CourseState extends State<Course> {
+  final List<String> courseList = ['DSA', 'Flutter', 'Python', 'Django'];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
-      child: Column(
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('Courses'),
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
         children: [
+          const SizedBox(height: 20),
           Center(
-            child: CustomTypewriterText(text: "Course part is Under Development", fontSize: 30, color: Colors.green),
-          )
+            child: CustomTypewriterText(
+              text: "Course part is Under Development",
+              fontSize: 24,
+              color: Colors.green,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: courseList.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Colors.grey[900],
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: ListTile(
+                    leading: const Icon(Icons.book, color: Colors.green),
+                    title: Text(
+                      courseList[index],
+                      style: const TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    trailing: const Icon(Icons.arrow_forward_ios, color: Colors.green),
+                    onTap: () {
+                      // TODO: Navigate to course detail page
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
